8ee753055e43d1563aa8f06feb7704bf2a923e56
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2025-02-08 23:53:11 +0530

    solved 1 prob of 1000

---
+++ 1000-rated/2. Raspberries.cpp
@@ -0,0 +1,131 @@
+// HARE RAM HARE KRISHNA
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const double PI = 3.14159265358979323846;
+#define INF (int)1e18
+#define f first
+#define s second
+#define pb(a) push_back(a)
+#define all(x) (x).begin(), (x).end()
+#define Ankur_Dwivedi()                   \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);                      \
+    }
+
+#define couty cout << "YES" << endl
+#define coutn cout << "NO" << endl
+#define cout(a) cout << a << endl
+
+#define ain(i, arr, n)          \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> arr[i];          \
+    }
+#define aout(i, arr, n)         \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << arr[i] << " ";  \
+    }
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+string binary(int x)
+{
+    string s = "";
+    for (int i = 31; i >= 0; i--)
+    {
+        int a = ((x >> i) & 1);
+        if (a == 1)
+        {
+            s += '1';
+        }
+        else
+        {
+            s += '0';
+        }
+    }
+    return s;
+}
+
+bool SieveOfEratosthenes(int n)
+{
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++)
+    {
+
+        if (prime[p] == true)
+        {
+
+            for (int i = p * p; i <= n; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+
+    if (prime[n])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void solve()
+{
+    int M = 1e9 + 7;
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    ain(i, a, n);
+    int ans = k;
+    int p = 1;
+    int ct = 0;
+    for (int i = 0; i < n; i++)
+    {
+        int rem = a[i] % k;
+        if (rem != 0)
+            rem = k - rem;
+        ans = min(ans, rem);
+        if (a[i] % 2 == 0)
+            ct++;
+    }
+    if (k == 4 && ct >= 2)
+    {
+        cout(0);
+        return;
+    }
+    else if (k == 4)
+    {
+        ans = (ans + 1) / 2;
+    }
+    cout(ans);
+}
+
+int32_t main()
+{
+    Ankur_Dwivedi();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
