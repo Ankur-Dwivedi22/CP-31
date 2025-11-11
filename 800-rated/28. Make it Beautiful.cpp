1338f6f6c7151f40fad1aa7b6ea3568c225c9151
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2025-11-11 21:30:35 +0530

    800 rated 27 & 28

---
+++ 800-rated/28. Make it Beautiful.cpp
@@ -0,0 +1,152 @@
+// HARE RAM HARE KRISHNA
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const double PI = 3.14159265358979323846;
+#define INF (int)1e18
+#define f first
+#define s second
+#define pb(a) push_back(a)
+#define all(x) (x).begin(), (x).end()
+#define Ankur_Dwivedi()                   \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);                      \
+    }
+
+#define couty cout << "YES" << endl
+#define coutn cout << "NO" << endl
+#define cout(a) cout << a << endl
+
+#define ain(i, arr, n)          \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> arr[i];          \
+    }
+#define aout(i, arr, n)         \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << arr[i] << " ";  \
+    }
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+string binary(int x)
+{
+    string s = "";
+    for (int i = 31; i >= 0; i--)
+    {
+        int a = ((x >> i) & 1);
+        if (a == 1)
+        {
+            s += '1';
+        }
+        else
+        {
+            s += '0';
+        }
+    }
+    return s;
+}
+
+bool SieveOfEratosthenes(int n)
+{
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++)
+    {
+
+        if (prime[p] == true)
+        {
+
+            for (int i = p * p; i <= n; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+
+    if (prime[n])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    ain(i, a, n);
+    sort(all(a));
+
+    vector<int> ans;
+    int sum = 0;
+    int l = 0, r = n - 1;
+    while (l < r)
+    {
+        if (a[l] == sum)
+        {
+            coutn;
+            return;
+        }
+        else
+        {
+            ans.pb(a[l]);
+            sum += a[l++];
+        }
+
+        if (a[r] == sum)
+        {
+            coutn;
+            return;
+        }
+        else
+        {
+            ans.pb(a[r]);
+            sum += a[r--];
+        }
+    }
+    if (l == r)
+    {
+        if (a[l] == sum)
+        {
+            coutn;
+            return;
+        }
+        else
+        {
+            ans.pb(a[l]);
+            sum += a[l++];
+        }
+    }
+
+    couty;
+    aout(i, ans, n);
+    cout<<"\n";
+}
+
+int32_t main()
+{
+    Ankur_Dwivedi();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
