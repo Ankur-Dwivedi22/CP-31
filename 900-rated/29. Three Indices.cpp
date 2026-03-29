02f20aeafb2df0d6acaa988a130f5ab9965a43bc
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-03-29 17:49:50 +0530

    900 rated q29

---
+++ 900-rated/29. Three Indices.cpp
@@ -0,0 +1,131 @@
+// HARE RAM HARE KRISHNA
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const double PI = 3.14159265358979323846;
+#define INF (int)1e18
+#define f first
+#define s second
+#define pb(a) push_back(a)
+#define all(x) (x).begin(), (x).end()
+#define Ankur_Dwivedi()                   \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);                      \
+    }
+
+#define couty cout << "YES" << endl
+#define coutn cout << "NO" << endl
+#define cout(a) cout << a << endl
+
+#define ain(i, arr, n)          \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> arr[i];          \
+    }
+#define aout(i, arr, n)         \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << arr[i] << " ";  \
+    }
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+string binary(int x)
+{
+    string s = "";
+    for (int i = 31; i >= 0; i--)
+    {
+        int a = ((x >> i) & 1);
+        if (a == 1)
+        {
+            s += '1';
+        }
+        else
+        {
+            s += '0';
+        }
+    }
+    return s;
+}
+
+bool SieveOfEratosthenes(int n)
+{
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++)
+    {
+
+        if (prime[p] == true)
+        {
+
+            for (int i = p * p; i <= n; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+
+    if (prime[n])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> p(n);
+    ain(i, p, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int j;
+        for (j = i + 1; j < n; j++)
+        {
+            if (p[j] > p[i])
+            {
+                break;
+            }
+        }
+        for (int k = j + 1; k < n; k++)
+        {
+            if (p[k] < p[j])
+            {
+                couty;
+                cout << i+1 << " " << j + 1 << " " << k + 1 << "\n";
+                return;
+            }
+        }
+    }
+
+    coutn;
+}
+
+int32_t main()
+{
+    Ankur_Dwivedi();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
