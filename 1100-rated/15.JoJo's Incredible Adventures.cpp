f63270f6f2eb8c52e57b7751726ab856cc397c40
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-05-03 00:24:40 +0530

    1100 rated

---
+++ 1100-rated/15.JoJo's Incredible Adventures.cpp
@@ -0,0 +1,124 @@
+// HARE RAM HARE KRISHNA
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const double PI = 3.14159265358979323846;
+#define INF (int)1e18
+#define f first
+#define s second
+#define pb(a) push_back(a)
+#define all(x) (x).begin(), (x).end()
+#define Ankur_Dwivedi()                   \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);                      \
+    }
+
+#define couty cout << "YES" << endl
+#define coutn cout << "NO" << endl
+#define cout(a) cout << a << endl
+
+#define ain(i, arr, n)          \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> arr[i];          \
+    }
+#define aout(i, arr, n)         \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << arr[i] << " ";  \
+    }
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+string binary(int x)
+{
+    string s = "";
+    for (int i = 31; i >= 0; i--)
+    {
+        int a = ((x >> i) & 1);
+        if (a == 1)
+        {
+            s += '1';
+        }
+        else
+        {
+            s += '0';
+        }
+    }
+    return s;
+}
+
+bool SieveOfEratosthenes(int n)
+{
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++)
+    {
+
+        if (prime[p] == true)
+        {
+
+            for (int i = p * p; i <= n; i += p)
+            {
+                prime[i] = false;
+            }
+        }
+    }
+
+    if (prime[n])
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+    int n = s.size();
+    string temp = s.substr(0, n - 1);
+    s += temp;
+    int mx = 0, ct = 0;
+    for (char c : s)
+    {
+        if (c == '1')
+        {
+            ct++;
+        }
+        else
+        {
+            ct = 0;
+        }
+        mx = max(mx, ct);
+    }
+    int ans = ((mx + 1) / 2) * 1ll * ((mx + 2) / 2);
+    cout(ans);
+}
+
+int32_t main()
+{
+    Ankur_Dwivedi();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
