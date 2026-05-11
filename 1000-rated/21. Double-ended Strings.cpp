5fd6b33ea18ddb3959ad093d3ab945ec8868e394
Ankur-Dwivedi22 <ankur.2201005cs@iiitbh.ac.in>
2026-05-11 22:44:47 +0530

    1000 rated

---
+++ 1000-rated/21. Double-ended Strings.cpp
@@ -0,0 +1,112 @@
+// HARE RAM HARE KRISHNA
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+const double PI = 3.14159265358979323846;
+#define INF (int)1e18
+#define f first
+#define s second
+#define pb(a) push_back(a)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define Ankur_Dwivedi()                   \
+    {                                     \
+        ios_base::sync_with_stdio(false); \
+        cin.tie(0);                       \
+        cout.tie(0);                      \
+    }
+
+#define couty cout << "YES" << endl
+#define coutn cout << "NO" << endl
+#define cout(a) cout << a << endl
+
+#define ain(i, arr, n)          \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cin >> arr[i];          \
+    }
+#define aout(i, arr, n)         \
+    ;                           \
+    for (int i = 0; i < n; i++) \
+    {                           \
+        cout << arr[i] << " ";  \
+    }
+
+using namespace std;
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+string binary(int x)
+{
+    string s = "";
+    for (int i = 31; i >= 0; i--)
+    {
+        int a = ((x >> i) & 1);
+        if (a == 1)
+        {
+            s += '1';
+        }
+        else
+        {
+            s += '0';
+        }
+    }
+    return s;
+}
+
+const int MAXN = 10000000;
+std::bitset<MAXN + 1> is_prime;
+
+void SieveOfEratosthenes()
+{
+    is_prime.set();
+    is_prime[0] = is_prime[1] = 0;
+
+    for (int p = 2; p * p <= MAXN; p++)
+    {
+        if (is_prime[p])
+        {
+            for (int i = p * p; i <= MAXN; i += p)
+            {
+                is_prime[i] = 0;
+            }
+        }
+    }
+}
+
+void solve()
+{
+    string a, b;
+    cin>>a>>b;
+    int n1 = a.size(), n2 = b.size();
+    int ans = 1e9;
+
+    for(int len=0; len<=min(n1, n2); len++){
+        for(int i=0; i<=n1-len; i++){
+            for(int j=0; j<=n2-len; j++){
+                if(a.substr(i, len) == b.substr(j, len)){
+                    ans = min(ans, n1+n2-2*len);
+                }
+            }
+        }
+    }
+
+    cout(ans);
+}
+
+int32_t main()
+{
+    Ankur_Dwivedi();
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
